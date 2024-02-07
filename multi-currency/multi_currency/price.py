e40201ce8155c76709f765bee9f847774ca83744
marcantoinec <macoindreau@hopia.eu>
2024-02-07 14:27:49 +0100

    implement addition between two currencies.

--- multi-currency/multi_currency/price.py
+++ multi-currency/multi_currency/price.py
@@ -4,10 +4,14 @@ class Price:
         self._currency = currency
 
     def __eq__(self, other):
-        return self._amount == other._amount and self._currency == other._currency
+        if self._currency == other._currency:
+            return self._amount == other._amount and self._currency == other._currency
+        return self == other.convert_to(self._currency)
 
     def __add__(self, other):
-        return Price(self._amount + other._amount, self._currency)
+        if self._currency == other._currency:
+            return Price(self._amount + other._amount, self._currency)
+        return self + other.convert_to(self._currency)
 
     def __rmul__(self, other):
         return Price(self._amount * other, self._currency)
@@ -18,4 +22,3 @@ class Price:
     def convert_to(self, new_currency: str):
         rates = {("USD", "EUR"): 0.5, ("EUR", "USD"): 2}
         return Price(self._amount * rates[(self._currency, new_currency)], new_currency)
-
--- multi-currency/tests/test_multi_currency.py
+++ multi-currency/tests/test_multi_currency.py
@@ -4,15 +4,24 @@ from multi_currency.price import Price
 def test_equal_dollar():
     assert Price(1, "USD") == Price(1, "USD")
 
+
 def test_not_equal_different_currencies():
     assert Price(1, "EUR") != Price(1, "USD")
 
+
 def test_add_same_currencies():
     assert Price(2, "EUR") == Price(1, "EUR") + Price(1, "EUR")
+
 
 def test_mult_same_currencies():
     assert Price(2, "EUR") == 2 * Price(1, "EUR")
     assert Price(2, "EUR") == Price(1, "EUR") * 2
 
-def test_conversion_rate_2(): # 1 EUR = 2 USD
+
+def test_conversion_rate_2():  # 1 EUR = 2 USD
     assert Price(1, "USD").convert_to("EUR") == Price(0.5, "EUR")
+
+
+def test_add_different_currencies():
+    assert Price(1, "USD") + Price(1, "EUR") == Price(3, "USD")
+    assert Price(1, "USD") + Price(1, "EUR") == Price(1.5, "EUR")
